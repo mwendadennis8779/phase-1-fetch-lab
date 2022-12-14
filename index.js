808348085a7c08e9a193b4517cda65719423a958
Dennis <mwendadennis8779@gmail.com>
2022-12-14 14:54:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resposne => resposne.json())
+  .then(books => renderBooks(books)) 
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    h2.innerHTML = `<p>${book.name}</p>`;
+    document.querySelector('main').appendChild(h2)
+    
   });
 }
+renderBooks()
+function initialize(){
+  fetchBooks()
+}
+initialize()
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
